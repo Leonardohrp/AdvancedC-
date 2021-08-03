6858eb2eab0bb2ea64aafaaa876937ffcbd8b78c
Leonardo Pinheiro <leonardohrp@hotmail.com>
2021-08-03 19:13:15 -0300

    BuilderPattern

---
+++ BuilderPattern.Demo/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuilderPattern.Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BuilderPattern.Demo", Version = "v1" });
+            });
+
+            //The Builder design pattern is one of the creational design patterns. 
+            //This pattern is not very common but very used in scenarios where the creation of an object can become complex.
+            services.AddSingleton<IUser>(new User.UserBuilder()
+                .WithName("Leo")
+                .WithAddress("Rua a")
+                .Build());
+
+            services.AddSingleton<IUser>(new User.UserBuilder()
+               .WithName("Heitor")
+               .WithAge(5)
+               .Build());
+
+            services.AddSingleton<IUser>(new User.UserBuilder()
+               .WithAge(5)
+               .Build());
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BuilderPattern.Demo v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
