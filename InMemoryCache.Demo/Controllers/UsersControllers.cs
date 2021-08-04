8e2f742289aa62f67b0888191174acaa058a8235
Leonardo Pinheiro <leonardohrp@hotmail.com>
2021-08-03 22:05:33 -0300

    InMemoryCache

---
+++ InMemoryCache.Demo/Controllers/UsersControllers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InMemoryCache.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersControllers : ControllerBase
+    {
+        private readonly IUserCache userCache;
+
+        public UsersControllers(IUserCache userCache)
+        {
+            this.userCache = userCache;
+        }
+
+        // GET: api/<UsersControllers>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return userCache.GetCachedUsers();
+        }
+    }
+}
---
+++ InMemoryCache.Demo/UserCache.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMemoryCache.Demo
+{
+    public class UserCache : IUserCache
+    {
+        private const string KEY = "user_cache";
+        private readonly IMemoryCache memoryCache;
+
+        public UserCache(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public void AddToCache(User[] users)
+        {
+            var option = new MemoryCacheEntryOptions
+            {
+                SlidingExpiration = TimeSpan.FromSeconds(30),
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(90)
+            };
+
+            // Get data from a database
+            memoryCache.Set<User[]>(KEY, users, option);
+        }
+
+        public User[] GetCachedUsers()
+        {
+            User[] users;
+            if (!memoryCache.TryGetValue(KEY, out users))
+            {
+                users = new User[] {
+                    new User{ Name = "Leo1", Age = 22 },
+                    new User{ Name = "Leo2", Age = 23 },
+                    new User{ Name = "Leo3", Age = 26 }
+                };
+                AddToCache(users);
+            }
+            return memoryCache.Get<User[]>(KEY);
+        }
+    }
+}
